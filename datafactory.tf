a612854570151d7a4c4906cc4333ea664e5ab4a3
bharath <bharathcg7@gmail.com>
2024-11-09 23:25:18 +0000

    airbus

--- datafactory.tf
+++
@@ -1,5 +0,0 @@
-resource "azurerm_data_factory" "aerodf" {
-  name                = "aero"
-  location            = azurerm_resource_group.airbus.location
-  resource_group_name = azurerm_resource_group.airbus.name
-}
--- main.tf
+++ main.tf
@@ -7,5 +7,5 @@ provider "azurerm" {
 # Define the resource group
 resource "azurerm_resource_group" "airbus" {
   name     = "airbus"
-  location = ""
+  location = "UKSouth"
 }
