152ad85d28bb0666fc7f03136ca31f4830b2de59
bharath <bharathcg7@gmail.com>
2024-11-10 00:18:22 +0000

    airbus

--- datafactory.tf
+++ datafactory.tf
@@ -1,4 +1,4 @@
-resource "azurerm_data_factory" "airbusdf" {
+resource "azurerm_data_factory" "planedf" {
   name                = "plane"
   location            = var.myvar1
   resource_group_name = azurerm_resource_group.airbus.name
